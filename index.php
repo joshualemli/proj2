557d31b2fbcdb817224f13227b90eeaa906e8da5
joshualemli <joshualemli@gmail.com>
2017-01-08 15:26:24 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>proj2</title>
+    <link type='text/css' rel='stylesheet' href='proj2.css'>
+    <script src='proj2.js'></script>
+  </head>
+  <body>
+    <div id='canvas'></div>
+  </body>
+</html>
---
+++ proj2.js
@@ -0,0 +1,17 @@
+"use strict";
+var proj2 = (function(){
+  
+  function rI(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  var canvas;
+  var context;
+  var entities = [];
+
+  window.onload = function() {
+    canvas = document.getElementById('canvas');
+    context = canvas.get
+  };
+
+})();
